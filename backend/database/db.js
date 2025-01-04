b70921a00cf3a1417bbba1c27742f712cd5811cc
Shreya Nigam <shreya.nigam1062@gmail.com>
2025-01-04 15:21:17 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE_NAME,
+  connectionString: process.env.DATABASE_URL, 
+  ssl: {
+    rejectUnauthorized: false, // Required for connecting to Render's managed PostgreSQL
+  },
 });
 
 module.exports = pool;
